c480d72833147624e4dce981736e57108800d077
Waldo Bronchart <wbronchart@gmail.com>
2013-01-08 20:45:41 +0000

    camera settings setup before run

--- pull_compile_run.sh
+++ pull_compile_run.sh
@@ -1,4 +1,32 @@
+# Pull and build
 git pull origin
 make
-#uvcdynctrl -s 'Exposure, Auto Priority' -- 0
+
+# Setup
+uvcdynctrl -s "Brightness" 118
+uvcdynctrl -s "Contrast" 30
+uvcdynctrl -s "Saturation" 73
+uvcdynctrl -s "White Balance Temperature, Auto" 0
+uvcdynctrl -s "Gain" 20
+uvcdynctrl -s "White Balance Temperature" 8360
+uvcdynctrl -s "Sharpness" 0
+uvcdynctrl -s "Backlight Compensation" 1
+uvcdynctrl -s "Exposure, Auto" -4
+uvcdynctrl -s "Exposure (Absolute)" 166
+uvcdynctrl -s "Exposure, Auto Priority" 0
+
+# Output settings
+uvcdynctrl -g "Brightness"
+uvcdynctrl -g "Contrast"
+uvcdynctrl -g "Saturation"
+uvcdynctrl -g "White Balance Temperature, Auto"
+uvcdynctrl -g "Gain"
+uvcdynctrl -g "White Balance Temperature"
+uvcdynctrl -g "Sharpness"
+uvcdynctrl -g "Backlight Compensation"
+uvcdynctrl -g "Exposure, Auto"
+uvcdynctrl -g "Exposure (Absolute)"
+uvcdynctrl -g "Exposure, Auto Priority"
+
+# Start the server
 sudo ./AmbiLightServer
