3604c4e9d65b9cf6f9e66e0e46d83c540210483f
Waldo Bronchart <wbronchart@gmail.com>
2013-02-17 20:32:11 +0000

    default camera settings

--- pull_compile_run.sh
+++ pull_compile_run.sh
@@ -6,12 +6,12 @@ make
 #uvcdynctrl -s "Brightness" 118
 #uvcdynctrl -s "Contrast" 30
 #uvcdynctrl -s "Saturation" 73
-uvcdynctrl -s "White Balance Temperature, Auto" 0
+#uvcdynctrl -s "White Balance Temperature, Auto" 0
 #uvcdynctrl -s "Gain" 20
-uvcdynctrl -s "White Balance Temperature" 8360
+#uvcdynctrl -s "White Balance Temperature" 8360
 #uvcdynctrl -s "Sharpness" 0
 #uvcdynctrl -s "Backlight Compensation" 1
-uvcdynctrl -s "Exposure, Auto" 3
+#uvcdynctrl -s "Exposure, Auto" 3
 #uvcdynctrl -s "Exposure (Absolute)" 166
 #uvcdynctrl -s "Exposure, Auto Priority" 0
 
