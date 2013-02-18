d9c24ae99355f3cc196db09a215cd57923f588a3
Waldo Bronchart <wbronchart@gmail.com>
2013-02-18 19:13:47 +0000

    An other lerpColor bug

--- Color.cpp
+++ Color.cpp
@@ -4,8 +4,8 @@
 Color lerpColor(const Color& c1, const Color& c2, float term)
 {
 	Color result;
-	result.R = (char)lerp(c1.R/255.0f, c2.R/255.0f, term);
-	result.G = (char)lerp(c1.G/255.0f, c2.G/255.0f, term);
-	result.B = (char)lerp(c1.B/255.0f, c2.B/255.0f, term);
+	result.R = (char)(lerp(c1.R/255.0f, c2.R/255.0f, term)*255);
+	result.G = (char)(lerp(c1.G/255.0f, c2.G/255.0f, term)*255);
+	result.B = (char)(lerp(c1.B/255.0f, c2.B/255.0f, term)*255);
 	return result;
 }
